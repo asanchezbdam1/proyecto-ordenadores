a231124e00d50aee84fad9d3b917b105c1fb7c3a
asanchezbdam1 <asanchezbdam1@gmail.com>
2021-05-28 19:16:53 +0200

    Añadido el cálculo de precio del ordenador

--- src/Ordenador.java
+++ src/Ordenador.java
@@ -13,7 +13,10 @@ public class Ordenador extends Articulo {
 
     public Ordenador(String usuario, double precio) {
         super(precio);
-        idOrdenador = usuario + java.time.LocalDate.now().toString();
+        discos = new ArrayList<>();
+        rams = new ArrayList<>();
+        gpus = new ArrayList<>();
+        setIdOrdenador(usuario + java.time.LocalDate.now().toString());
     }
 
     public boolean addDisco(Disco disco) {
@@ -41,22 +44,85 @@ public class Ordenador extends Articulo {
     }
 
     public boolean addGpu(Gpu gpu) {
+    	if (placaBase.getSlotsPCIe() > 0) {
+    		gpus.add(gpu);
+    	}
         return false;
     }
 
     public void removeDisco(Disco disco) {
-
+    	discos.remove(disco);
     }
 
     public void removeGpu(Gpu gpu) {
-
+    	gpus.remove(gpu);
     }
 
     public void removeRam(Ram ram) {
-
+    	rams.remove(ram);
     }
 
-    public double calcularPrecio() {
-        return 0;
+    private double calcularPrecio() {
+    	double precio = 0;
+    	if (placaBase != null) {
+    		precio += placaBase.getPrecio();
+    	}
+    	if (fuente != null) {
+    		precio += fuente.getPrecio();
+    	}
+    	if (torre != null) {
+    		precio += torre.getPrecio();
+    	}
+    	if (cpu != null) {
+    		precio += cpu.getPrecio();
+    	}
+    	for (Disco d : discos) {
+    		precio += d.getPrecio();
+    	}
+    	for (Ram r : rams) {
+    		precio += r.getPrecio();
+    	}
+    	for (Gpu g : gpus) {
+    		precio += g.getPrecio();
+    	}
+        return precio;
     }
+
+	public String getIdOrdenador() {
+		return idOrdenador;
+	}
+
+	public void setIdOrdenador(String idOrdenador) {
+		this.idOrdenador = idOrdenador;
+	}
+
+	public Cpu getCpu() {
+		return cpu;
+	}
+
+	public void setCpu(Cpu cpu) {
+		this.cpu = cpu;
+	}
+
+	public Fuente getFuente() {
+		return fuente;
+	}
+
+	public void setFuente(Fuente fuente) {
+		this.fuente = fuente;
+	}
+
+	public Torre getTorre() {
+		return torre;
+	}
+
+	public void setTorre(Torre torre) {
+		this.torre = torre;
+	}
+	
+	public void setPrecio(double precio) {}
+	
+	public double getPrecio() {
+		return calcularPrecio();
+	}
 }
