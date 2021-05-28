a0e64ceb437b1673609b37282e91e598e32724ca
aguallarcdam1 <aguallarcdam1@gmail.com>
2021-05-28 19:36:07 +0200

    Cambios en pedidos

--- src/Pedidos.java
+++ src/Pedidos.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedidos {
-    
+	private String idCliente;
+	private List<Pedido> pedidos;
+
+	public Pedidos() {
+		this.pedidos = new ArrayList<>();
+	}
+
+	public String getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(String idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public void addPedidos(Pedido pedido) {
+		pedidos.add(pedido);
+	}
+
+	public void modificarPedido(Pedido pedido) {
+
+	}
+
 }
